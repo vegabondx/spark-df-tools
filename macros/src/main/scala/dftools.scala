0ac889ec4780a254ab76d82e4f3ff0f41ccc9220
vegabondx <abhijit.oka@hotmail.com>
2022-07-06 08:59:21 -0400

    adding files

---
+++ macros/src/main/scala/dftools.scala
@@ -0,0 +1,27 @@
+package com.github.vegabondx.dftools
+
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.DataFrame
+
+object Functions {
+
+  def seqToCol(columnNames: Seq[String]) = columnNames.map(c => col(c))
+  def seqToColDf(df: DataFrame, columnNames: Seq[String]) =
+    columnNames.map(c => df(c))
+
+  def joinExpr(keys: Seq[String], df1: DataFrame, df2: DataFrame) =
+    keys.map { case (c1) => df1(c1) === df2(c1) }.reduce(_ && _)
+  def joinExprNs(keys: Seq[String], df1: DataFrame, df2: DataFrame) =
+    keys.map { case (c1) => df1(c1) <=> df2(c1) }.reduce(_ && _)
+
+  def addSuffix(
+      df: DataFrame,
+      suffix: String,
+      pk: Seq[String] = Seq()
+  ): DataFrame = {
+    val renamedColumns = df.columns.map(c =>
+      if (pk.contains(c)) df(c) else df(c).as(s"${c}_$suffix")
+    )
+    df.select(renamedColumns: _*)
+  }
+}
--- macros/src/main/scala/functions.scala
+++
@@ -1,9 +0,0 @@
-package com.dftools
-
-import org.apache.spark.sql.functions.{col, lit, unix_timestamp, month, year}
-import org.apache.spark.sql.DataFrame
-
-object Functions {
-  def seqToCol (columnNames: Seq[String] ) = columnNames.map (c => col(c) )
-
-}
